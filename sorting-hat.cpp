5b36cecd92bb47c679eb307571a547648e4938d2
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-02-06 07:04:53 -0600

    Add variables for houses and answers + welcome msg

--- sorting-hat.cpp
+++ sorting-hat.cpp
@@ -3,4 +3,17 @@
 int main() {
 
     // Time to get sorted, punk.
+    int gryffindor = 0;
+    int hufflepuff = 0;
+    int ravenclaw = 0;
+    int slytherin = 0;
+
+    // Answer variables
+    int answer1;
+    int answer2;
+    int answer3;
+    int answer4;
+
+    // Welcome message
+    std::cout << "The Sorting Hat Quiz\n";
 }
